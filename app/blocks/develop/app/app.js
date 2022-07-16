37983d304fda3c4fb0bcca2427a2e1abb8b1cb2d
Yrii Hvozdetskiy <hvozdetskiyyrii@gmail.com>
2022-07-16 20:33:47 +0300

    add burger and hamburger menu

--- app/blocks/develop/app/app.js
+++ app/blocks/develop/app/app.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
 	console.log('ready')
 	handleTabs()
-	searchElDropdown()
+	searchElDropdown();
+	handleBurgerMenu();
 })
 
 $(window).on('load', function () {
-	console.log('load')
-})
-
+	console.log('load');
+});
 
 // ========= стилі написання JS функцій (обгортаємо все в функцію) ============
 
 const handleTabs = () => {
-	$(document).on('click', '.look__tabs', () => {
+	$(document).on('click', '.look__tabs', function () {
 		// щось робем
-	})
-}
+	});
+};
+
+const handleBurgerMenu = () => {
+
+	$(document).on('click', '.burger-menu', function () {
+		$(this).toggleClass('open');
+	});
+};
