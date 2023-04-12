4060778d470f44b13493fb08e08654a89691bbf7
rasty <hvozdetskiyyrii@gmail.com>
2023-04-12 16:54:42 +0300

    replace README, add CDN plugins

--- app/blocks/develop/app/deps.js
+++ app/blocks/develop/app/deps.js
@@ -15,21 +15,34 @@ module.exports = {
       // 	from: 'node_modules/@fancyapps/ui/dist',// подключим fancybox из node_modules, буде на всіх сторінках в всіх css файлах і в ФІНАЛЬНІЙ збірці теж (npm run do)
       // 	import: ['fancybox.css'],
       // }
-      // якщо потрібно підключити js бібліотеки асинхроно то через CDN
+
+      // якщо потрібно підключити js бібліотеки через async, defer н-д: jquery.min.js@async, jquery.min.js@defer
+
+      // =================== CDN ===================
+
+       //   ===== jquery =====
+      {
+      	from: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/',
+      	inject: ['jquery.min.js'],
+      },
+      //    ===== swiper =====
       // {
-      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/',
-      // 	inject: ['jquery.min.js'],
-      // },
-      // //  ===== swiper =====
-      // {
-      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.2/',
+      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.2.0/',
       // 	inject: ['swiper-bundle.min.js','swiper-bundle.min.css'],
       // },
-      // //  ===== fancybox =====
+
+      //    ===== fancybox =====
       // {
-      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/',
-      // 	inject: ['jquery.fancybox.min.js','jquery.fancybox.min.css'],
+      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/fancyapps-ui/5.0.15/fancybox/',
+      // 	inject: ['fancybox.umd.js','fancybox.min.css'],
       // },
+
+      //    ===== jquery-validate =====
+      // {
+      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/',
+      // 	inject: ['jquery.validate.min.js'],
+      // },
+
       // //  ===== jqueryui =====
       // {
       // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/',
@@ -39,15 +52,13 @@ module.exports = {
       // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/',
       // 	inject: ['jquery-ui.min.css'],
       // },
-      // //  ===== jquery-validate =====
+
+      // =================== / CDN ===================
+
       // {
-      // 	from: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/',
-      // 	inject: ['jquery.validate.min.js'],
+      //    from: 'app/blocks/develop/app/assets',
+      //    inject: ['jquery-3.6.0.min.js'],
       // },
-      {
-         from: 'app/blocks/develop/app/assets',
-         inject: ['jquery-3.6.0.min.js'],
-      },
       // {
       //    from: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/',
       //    inject: ['lodash.min.js'],
@@ -60,28 +71,28 @@ module.exports = {
       //    from: 'app/blocks/develop/app/assets',
       //    inject: ['lodash_debounce.js'],
       // },
-      {
-         from: 'app/blocks/develop/app/plugins/swiper',
-         inject: ['swiper.min.js', 'swiper.min.css'],
-      },
-      {
-         from: 'app/blocks/develop/app/plugins/fancybox',
-         inject: ['jquery.fancybox.min.js', 'fancybox.css'],
-      },
+      // {
+      //    from: 'app/blocks/develop/app/plugins/swiper',
+      //    inject: ['swiper.min.js', 'swiper.min.css'],
+      // },
+      // {
+      //    from: 'app/blocks/develop/app/plugins/fancybox',
+      //    inject: ['jquery.fancybox.min.js', 'fancybox.css'],
+      // },
       // {
       // 	from: 'app/blocks/develop/app/plugins/jquery-ui',
       // 	inject: ['jquery-ui.min.js', 'jquery-ui.min.css'],
       // },
-      {
-         from: 'app/blocks/develop/app/plugins/validate',
-         inject: ['jquery.validate.min.js'],
-         // function filter - можем підключати файли тільки на ту сторінку яка нам потрібна
-         filter(file, node, type, page) {
-            // проверка includes( '[імя сторіки на якій підключати файл]' )
-            // пишем в page <div class="app app_no_js [index]">
-            return node.attrs.class.split(' ').includes('index'); // буде підключений тільки на сторінці 'index'
-         },
-      },
+      // {
+      //    from: 'app/blocks/develop/app/plugins/validate',
+      //    inject: ['jquery.validate.min.js'],
+      //    // function filter - можем підключати файли тільки на ту сторінку яка нам потрібна
+      //    filter(file, node, type, page) {
+      //       // проверка includes( '[імя сторіки на якій підключати файл]' )
+      //       // пишем в page <div class="app app_no_js [index]">
+      //       return node.attrs.class.split(' ').includes('index'); // буде підключений тільки на сторінці 'index'
+      //    },
+      // },
       //можем підключати в загаліний файл окремі файли з кодом для select_dropdown наприклад чи слайдера (всі ці файли будуть в одному app.js)
       {
          //from: 'app/blocks/develop/app', - якщо немає, то пошук буде йти з app/blocks/develop/app/assets
